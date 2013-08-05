f0b02bc113b983567503ca600e185d8571fbc465
Adam Reis <adamhreis@gmail.com>
2013-08-05 15:53:36 -0400

    blank xcode project

---
+++ venmo-touch-wrapper/venmo-touch-wrapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  venmo-touch-wrapper
+//
+//  Created by Adam Reis on 8/5/13.
+//  Copyright (c) 2013 Adam Reis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TouchWrapperAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TouchWrapperAppDelegate class]));
+    }
+}
